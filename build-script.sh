48fc8cd509b913bb4b469a848a550ed67d384c45
Mouse <mouse008@gmail.com>
2017-08-04 18:12:19 -0400

    Reduce parallel runs to 2 to prevent random failures in the process...

--- build-script.sh
+++ build-script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-botan-remove && make clean && make -j 4 && ./botan-test && ./botan-test pkcs11 --pkcs11-lib=/opt/local/lib/softhsm/libsofthsm2.so && sudo make install && sudo chown -R uri * && cd build/docs/manual/ && make && cp botan.pdf ~/SkyDrive/botan-2.0.pdf  && make clean && cd ../../.. && make clean
+botan-remove && make clean && make -j 2 && ./botan-test && ./botan-test pkcs11 --pkcs11-lib=/opt/local/lib/softhsm/libsofthsm2.so && sudo make install && sudo chown -R uri * && cd build/docs/manual/ && make && cp botan.pdf ~/SkyDrive/botan-2.0.pdf  && make clean && cd ../../.. && make clean
