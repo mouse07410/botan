dda0d796f0c78a9b86fde6824fc1271e99cb3bc1
Mouse <mouse008@gmail.com>
2017-12-06 22:44:09 -0500

    Make sure docs are properly built, and cleanup removes the doc.stamp (if not removed, it would prevent docs from being rebuilt next time)

--- build-script.sh
+++ build-script.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 botan-remove || true
-make clean && make -j 2 && ./botan-test --pkcs11-lib=/opt/local/lib/softhsm/libsofthsm2.so && sudo make install && sudo chown -R uri * && cd build/docs/manual/ && make && cp botan.pdf ~/Documents/botan-2.0.pdf  && make clean && cd ../../.. && make clean
+
+#make clean && make -j 2 all && ./botan-test --pkcs11-lib=/opt/local/lib/softhsm/libsofthsm2.so && sudo make install && sudo chown -R uri * && cd build/docs/manual/ && make && cp botan.pdf ~/Documents/botan-2.0.pdf  && make clean && cd ../../.. && make clean
+
+rm -f build/doc.stamp
+make clean && make -j 2 all && ./botan-test --pkcs11-lib=/opt/local/lib/softhsm/libsofthsm2.so && sudo make install && sudo chown -R uri * && cd build/docs/manual/ && cp botan.pdf ~/Documents/botan-2.0.pdf  && cd ../../.. && make clean && rm -f build/doc.stamp
