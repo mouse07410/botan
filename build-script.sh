3de0f2c0ea76957171811f899ae3c63cb410bb14
Mouse <mouse008@gmail.com>
2018-01-10 18:21:11 -0500

    Streamline automated build script

--- build-script.sh
+++ build-script.sh
@@ -4,4 +4,4 @@ botan-remove || true
 #make clean && make -j 2 all && ./botan-test --pkcs11-lib=/opt/local/lib/softhsm/libsofthsm2.so && sudo make install && sudo chown -R uri * && cd build/docs/manual/ && make && cp botan.pdf ~/Documents/botan-2.0.pdf  && make clean && cd ../../.. && make clean
 
 rm -f build/doc.stamp
-make clean && make -j 2 all && ./botan-test --pkcs11-lib=/opt/local/lib/softhsm/libsofthsm2.so && sudo make install && sudo chown -R uri * && cd build/docs/manual/ && cp botan.pdf ~/Documents/botan-2.0.pdf  && cd ../../.. && make clean && rm -f build/doc.stamp
+make clean && make -j 2 all && ./botan-test --pkcs11-lib=/opt/local/lib/softhsm/libsofthsm2.so && sudo make install && sudo chown -R uri * && cp build/docs/manual/botan.pdf ~/Documents/botan-2.0.pdf  && make clean && rm -f build/doc.stamp
