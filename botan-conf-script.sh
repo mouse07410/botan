646fd0a27f37787b2385520c039045fd7619841b
Mouse <mouse008@gmail.com>
2020-02-19 10:19:43 -0500

    Revert "Fix bug triggering crash on Catalina in ccrng_generate"
    This reverts commit 59f5d8fe16045a3c0977b8b0734a7a0ab00c3330.

--- botan-conf-script.sh
+++ botan-conf-script.sh
@@ -2,7 +2,11 @@
 
 #./configure.py --prefix=/opt/local --without-unaligned-mem --with-openssl --with-boost --with-lzma --with-bzip2 --with-zlib --with-sqlite3 --with-python-version=2.7 --with-sphinx --enable-modules=pkcs11 --cc-abi-flags='-maes -mpclmul -mrdrnd -msse2 -mssse3 -msse4 -msse4.2 -Os -Ofast -I/opt/local/include'
 
-./configure.py --debug --debug-mode --prefix=/opt/local --with-os-features=security_framework,apple_keychain,commoncrypto,getentropy --with-openmp --with-openssl --with-boost --with-lzma --with-bzip2 --with-zlib --with-sqlite3 --with-python-version=2.7 --with-sphinx --with-pdf --cc-abi-flags='-march=native -g -Os -Ofast -I/opt/local/include -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk' 2>&1 | tee conf-out.txt
+#./configure.py --prefix=/opt/local --with-commoncrypto --with-os-features=security_framework,commoncrypto,getentropy --with-openssl --with-boost --with-lzma --with-bzip2 --with-zlib --with-sqlite3 --with-python-version=2.7 --with-sphinx --with-pdf --cc-abi-flags='-march=native -Os -Ofast -I/opt/local/include' 2>&1 | tee conf-out.txt
+
+#./configure.py --prefix=/opt/local --with-os-features=security_framework,apple_keychain,commoncrypto,getentropy --with-openmp --with-openssl --with-boost --with-lzma --with-bzip2 --with-zlib --with-sqlite3 --with-python-version=2.7 --with-sphinx --with-pdf --cc-bin='/opt/local/bin/clang++-mp-8.0' --cc-abi-flags='-march=native -Os -Ofast -I/opt/local/include' 2>&1 | tee conf-out.txt
+
+./configure.py --prefix=/opt/local --with-os-features=security_framework,apple_keychain,commoncrypto,getentropy --with-openmp --with-openssl --with-boost --with-lzma --with-bzip2 --with-zlib --with-sqlite3 --with-python-version=2.7 --with-sphinx --with-pdf --cc-abi-flags='-march=native -Os -Ofast -I/opt/local/include' 2>&1 | tee conf-out.txt
 
 gsed -f botan-edit-makefile.sed < Makefile > t.mak
 mv t.mak Makefile
