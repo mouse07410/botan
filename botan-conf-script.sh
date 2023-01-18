6ef4b6b3328b579d26577127bb3710e6519b36b9
Mouse <mouse008@gmail.com>
2023-01-18 08:46:55 -0500

    Force -std=c++20, and -stdlib=libc++ to enable build by GCC

--- botan-conf-script.sh
+++ botan-conf-script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash -v
 
-CXXFLAGS="-Ofast -Os -I/opt/local/include -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
+CXXFLAGS="-std=c++20 -stdlib=libc++ -O3 -I/opt/local/include -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
 
 #./configure.py --prefix=/opt/local --with-os-features=security_framework,apple_keychain,commoncrypto,getentropy --with-openssl --with-boost --with-lzma --with-bzip2 --with-zlib --with-sqlite3 --with-python-version=3.9 --with-sphinx --with-pdf --cc-bin='/opt/local/bin/clang++-mp-12' --cc-abi-flags='-march=native -Os -Ofast -I/opt/local/include' 2>&1 | tee conf-out.txt
 
