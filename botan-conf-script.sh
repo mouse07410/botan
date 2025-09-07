9508ba0a02620f230ef768c25a1596f105a3bad6
Mouse <uri@mit.edu>
2025-09-07 12:27:39 -0400

    Move Boost version from 1.81 to 1.88

--- botan-conf-script.sh
+++ botan-conf-script.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash -v
 
-#CXXFLAGS="-std=c++20 -stdlib=libc++ -O3 -I/opt/local/libexec/boost/1.81/include -I/opt/local/include -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
-CPLUS_INCLUDE_PATH="/opt/local/libexec/boost/1.81/include"
-LIBRARY_PATH="/opt/local/libexec/boost/1.81/lib:/opt/local/lib"
-DISABL="-D_LIBCPP_DISABLE_AVAILABILITY"
+#CXXFLAGS="-std=c++20 -stdlib=libc++ -O3 -I/opt/local/libexec/boost/1.88/include -I/opt/local/include -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
+CPLUS_INCLUDE_PATH="/opt/local/libexec/boost/1.88/include"
+LIBRARY_PATH="/opt/local/libexec/boost/1.88/lib:/opt/local/lib"
+#DISABL="-D_LIBCPP_DISABLE_AVAILABILITY"
 BOTANLIBS="-lboost_thread-mt -lboost_random-mt -lboost_exception-mt -lboost_filesystem-mt -lboost_system-mt"
 
-CXXFLAGS="-std=c++20 -stdlib=libc++ -O3 ${DISABL} -I/opt/local/libexec/boost/1.81/include -I/opt/local/include"
+CXXFLAGS="-std=c++20 -stdlib=libc++ -O3 ${DISABL} -I/opt/local/libexec/boost/1.88/include -I/opt/local/include"
 
 # Need Macports-installed Clang-21 to build Botan-3 with SDK from XCode-13.2.1
 #./configure.py --prefix=/opt/local --program-suffix=3 --with-boost --cc-bin='/opt/local/bin/clang++-mp-21' --extra-cxxflags='-I/opt/local/libexec/llvm-21/include' --ldflags='-L/opt/local/libexec/llvm-21/lib -Wl,-rpath,/opt/local/libexec/llvm-21/lib' --with-os-features=security_framework,apple_keychain,commoncrypto,getentropy --with-commoncrypto --with-lzma --with-bzip2 --with-zlib --with-sqlite3 --with-python-version=3.11 --with-sphinx --with-pdf --system-cert-bundle=/opt/local/share/curl/curl-ca-bundle.crt --cc-abi-flags='-march=native'  2>&1 | tee conf-out.txt
--- botan-edit-makefile.sed
+++ botan-edit-makefile.sed
@@ -1,4 +1,4 @@
-/LIB_LINKS_TO/s/\=/\= -L\/opt\/local\/lib\/libomp -lomp -L\/opt\/local\/libexec\/boost\/1\.81\/lib /
+/LIB_LINKS_TO/s/\=/\= -L\/opt\/local\/lib\/libomp -lomp -L\/opt\/local\/libexec\/boost\/1\.88\/lib /
 /LIB_LINKS_TO/s/ -lsqlite3/ -L\/opt\/local\/lib -lsqlite3 /
 /LIB_LINKS_TO/s/system\ /system-mt\ /g
 /ABI_FLAGS/s/fopenmp/Xpreprocessor -fopenmp/
